3be1a63d5bf2eed9a3b103b05489e3a2668ecc4e
goaskoz <goaskoz@gmail.com>
2024-04-02 23:43:38 +0200

    1: edit flyToLocation all buttons

--- script.js
+++ script.js
@@ -50,4 +50,81 @@ var map = new mapboxgl.Map({
             });
     }
 
-    
+
+    // Fonction pour effectuer un mouvement FlyTo avec des paramètres spécifiques
+    function flyToLocation(zoom, center, pitch, bearing) {
+        map.flyTo({
+            zoom: zoom,
+            center: center,
+            pitch: pitch,
+            bearing: bearing
+        });
+    }
+
+    // Configuration des événements pour chaque bouton
+
+    document.getElementById('Manoir').addEventListener('click', function () {
+        flyToLocation(12, [-1.750997908366768, 48.024475510946225], 0, 0);
+    });
+
+    document.getElementById('Herverie').addEventListener('click', function () {
+        flyToLocation(12, [-1.750997908366768, 48.024475510946225], 0, 0);
+    });
+
+    document.getElementById('Archi').addEventListener('click', function () {
+        flyToLocation(12, [-1.749047374624584, 48.023139111604912], 0, 0);
+    });
+
+    document.getElementById('BruzCentre').addEventListener('click', function () {
+        flyToLocation(12, [-1.747579017093493, 48.02457091521449], 0, 0);
+    });
+
+    document.getElementById('GrandLogis').addEventListener('click', function () {
+        flyToLocation(12, [-1.745197283999986, 48.023350426665438], 0, 0);
+    });
+
+    document.getElementById('Cimetiere').addEventListener('click', function () {
+        flyToLocation(12, [-1.74383721428459, 48.023611492130541], 0, 0);
+    });
+
+    document.getElementById('VauGaillard').addEventListener('click', function () {
+        flyToLocation(12, [-1.744925249220352, 48.025107783866694], 0, 0);
+    });
+
+    document.getElementById('Providence').addEventListener('click', function () {
+        flyToLocation(12, [-1.748200453216257, 48.026062695182603], 0, 0);
+    });
+
+    document.getElementById('JacquesPrevert').addEventListener('click', function () {
+        flyToLocation(12, [-1.748437883909554, 48.026561149499976], 0, 0);
+    });
+
+    document.getElementById('2eRMAT').addEventListener('click', function () {
+        flyToLocation(12, [-1.749233693997537, 48.027826219789361], 0, 0);
+    });
+
+    document.getElementById('QuartierGare').addEventListener('click', function () {
+        flyToLocation(12, [-1.75031145978612, 48.027790984498168], 0, 0);
+    });
+
+    document.getElementById('GareBruz').addEventListener('click', function () {
+        flyToLocation(12, [-1.751391555444828, 48.028467414780486], 0, 0);
+    });
+
+    document.getElementById('Boissousel').addEventListener('click', function () {
+        flyToLocation(12, [-1.752644997570135, 48.025797765650523], 0, 0);
+    });
+
+    document.getElementById('Mine2GM').addEventListener('click', function () {
+        flyToLocation(12, [-1.74994231600716, 48.024082071504317], 0, 0);
+    });
+
+    document.getElementById('Presbytere').addEventListener('click', function () {
+        flyToLocation(12, [-1.748559802614701, 48.024049317640575], 0, 0);
+    });
+
+    document.getElementById('ChanoineRoullin').addEventListener('click', function () {
+        flyToLocation(12, [-1.747879264407958, 48.024153684658955], 0, 0);
+    });
+
+
