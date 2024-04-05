739d115fbba478ddaaec03afa2b0df351138b974
goaskoz <goaskoz@gmail.com>
2024-04-05 17:38:34 +0200

    update js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ document.addEventListener('DOMContentLoaded', function() {
           // Déplacez la carte vers les coordonnées correspondantes
           map.flyTo({
             center: coordonnees,
-            zoom: 15
+            zoom: 12
           });
         }
       });
